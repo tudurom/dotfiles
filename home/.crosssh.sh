59eb97296a8af7a02cb311f389ad3bb08cab5c3e
Tudor <tudurom@gmail.com>
2016-02-10 13:26:38 +0200

    Tmux

---
+++ home/.crosssh.sh
@@ -0,0 +1,46 @@
+export EDITOR=nvim
+export VISUAL=nvim
+export GOPATH=$HOME/Go
+export PATH="$(ruby -e 'print Gem.user_dir')/bin:$HOME/.scripts:.scripts/clint:$GOPATH/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+# Aliases aka hacks
+alias :q=exit
+alias irb=pry
+alias ccat=pygmentize
+alias problemhelper="time problemhelper"
+alias ph=problemhelper
+export LESS='-F -g -i -M -R -S -w -X -z-4'
+
+#source ~/.scripts/base16-ocean.dark.sh
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+
+# The Fuck is real
+eval $(thefuck --alias)
+
+#fortune | lolcat
+
+# Remove unnecessary packages
+pclean() {
+  sudo pacman -Rsn $(pacman -Qdtq)
+}
+
+testnet() {
+  while true; do
+    ping google.com
+  done
+}
+
+cless() {
+  pygmentize -f terminal "$1" | less -R
+}
+
+ghclone() {
+  git clone https://github.com/${1}.git
+}
+
+nvim() {
+  /usr/bin/nvim $@; clear
+}
+
