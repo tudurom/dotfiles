797383b751f4d953ff86ca0ff643e4cf0799d963
Tudor <xenogenesis@openmailbox.org>
2016-04-05 14:17:20 +0300

    rip lock.sh

--- scripts/.scripts/lock.sh
+++ scripts/.scripts/lock.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-# Pause if playing
-if [ "$(playerctl status)" = "Playing"  ] ; then
-  playerctl pause
-fi
-i3lock -u -i /tmp/screen.png
-systemctl suspend
-rm /tmp/screen.png
+#!/bin/dash
 
+exec slock
