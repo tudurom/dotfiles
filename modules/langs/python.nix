790d84a479f1e5bf932a13d78ad26fe9c30445b6
Tudor Roman <tudor@tudorr.ro>
2020-12-14 21:50:43 +0200

    python: added stuff for doom

--- modules/langs/python.nix
+++ modules/langs/python.nix
@@ -15,7 +15,9 @@ with lib; {
   config = mkIf cfg.enable {
     tudor.home.home.packages = with pkgs; [
       python38
-      #python38Packages.black
+      python38Packages.black
+      python38Packages.pyflakes
+      python38Packages.isort
       python38Packages.ipython
       python38Packages.pip
       python38Packages.pylint
