2e4387770d6ec9b1ac7973668035b9a16cdeeefd
Tudor Roman <tudor@tudorr.ro>
2020-12-14 21:24:50 +0200

    separated printing into module

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./erase-root.nix
+    ./printing.nix
   ];
 
   services.udev.extraRules = ''
---
+++ modules/system/printing.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.tudor.printing;
+in
+with lib; {
+  options.tudor.printing.enable = mkEnableOption "printing";
+
+  config = mkIf cfg.enable {
+    services.printing.enable = true;
+    services.printing.drivers = with pkgs; [
+      brgenml1lpr
+      brgenml1cupswrapper
+    ];
+    services.avahi.enable = true;
+    services.avahi.nssmdns = true;
+    services.avahi.publish.enable = true;
+    services.avahi.publish.userServices = true;
+  };
+}
