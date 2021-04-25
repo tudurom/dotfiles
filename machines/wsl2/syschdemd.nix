6b6a0c353c495d2bda5ff3d62a78dfd5f0e252c0
Tudor Roman <tudor@tudorr.ro>
2021-04-25 18:10:45 +0300

    this repo is now cursed: wsl support

---
+++ machines/wsl2/syschdemd.nix
@@ -0,0 +1,16 @@
+# Borrowed from github.com/Trundle/NixOS-WSL
+{ lib, pkgs, config, defaultUser, ... }:
+
+pkgs.substituteAll {
+  name = "syschdemd";
+  src = ./syschdemd.sh;
+  dir = "bin";
+  isExecutable = true;
+
+  buildInputs = with pkgs; [ daemonize ];
+
+  inherit (pkgs) daemonize;
+  inherit defaultUser;
+  inherit (config.security) wrapperDir;
+  fsPackagesPath = lib.makeBinPath config.system.fsPackages;
+}
---
+++ modules/desktop/minimal.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.tudor.graphicalSession.minimal;
+in
+with lib; {
+  options = {
+    tudor.graphicalSession.minimal.enable =
+      mkEnableOption "Graphical session without programs and with no display manager";
+  };
+
+  config = mkIf cfg.enable {
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-wlr xdg-desktop-portal-gtk ];
+    };
+
+    tudor.desktop = {
+      fonts.enable = true;
+      gtk.enable = true;
+    };
+
+    # No desktop without  s o u n d
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+
+    services.geoclue2.enable = true;
+    services.xserver.enable = true;
+
+    services.flatpak.enable = true;
+    programs.dconf.enable = true;
+  };
+}
