f76de7617634c620fc64546b7e6c2d43e7b086e0
Tudor Roman <tudor@tudorr.ro>
2021-10-02 15:34:20 +0300

    vscode with remote wsl extension works p e r f e c t l y

--- modules/system/nix-ld.nix
+++ modules/system/nix-ld.nix
@@ -4,4 +4,12 @@ let
 in
 {
   imports = [ "${sources.nix-ld}/modules/nix-ld.nix" ];
+
+  environment.variables = {
+    NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+      stdenv.cc.cc
+      gcc-unwrapped.lib
+    ];
+    NIX_LD = builtins.readFile "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+  };
 }
