2084e4d6f43db6db07c0b339644a83d529dc85b6
Tudor Roman <tudor@tudorr.ro>
2022-02-02 12:27:58 +0100

    use nix-direnv instead of lorri

--- modules/tools/nix.nix
+++ modules/tools/nix.nix
@@ -2,7 +2,10 @@
 {
   tudor.home = {
     home.packages = with pkgs; [ niv ];
-    programs.direnv.enable = true;
-    services.lorri.enable = true;
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      nix-direnv.enableFlakes = true;
+    };
   };
 }
