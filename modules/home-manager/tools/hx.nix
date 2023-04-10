4b3d224bb3670cf447fcff170da2bd758c7d505c
Tudor Roman <tudor@tudorr.ro>
2023-04-10 14:44:19 +0200

    Make hx config nicer

--- modules/home-manager/tools/hx.nix
+++ modules/home-manager/tools/hx.nix
@@ -20,8 +20,12 @@ with lib; {
       package = pkgs.unstable.helix;
       settings = {
         theme = "gruvbox_light";
+
         editor.bufferline = "multiple";
         editor.true-color = true;
+        editor.color-modes = true;
+
+        editor.indent-guides.render = true;
       };
     };
 
