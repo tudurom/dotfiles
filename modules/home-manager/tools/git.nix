7c56b60cc5b621d578dc1e9633b50f2ee0c6f863
Tudor Roman <tudor@tudorr.ro>
2023-06-04 20:21:03 +0200

    git: commit signing with ssh key

--- modules/home-manager/tools/git.nix
+++ modules/home-manager/tools/git.nix
@@ -22,7 +22,18 @@ with lib; {
           log --graph --color --pretty=format:"%C(yellow)%H%C(green)%d%C(reset)%n%x20%cd%n%x20%cn%x20(%ce)%n%x20%s%n"
         '';
       };
+
+      extraConfig = {
+        user.signingkey = removeSuffix "\n" (builtins.readFile ../../../id_ed25519.pub);
+        gpg.format = "ssh";
+        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        commit.gpgsign = true;
+      };
     };
+
+    home.file.".ssh/allowed_signers".text = ''
+      * ${builtins.readFile ../../../id_ed25519.pub}
+    '';
 
     home.packages = with pkgs; [
       git-lfs
