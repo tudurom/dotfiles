b11180b28d79aa6210c20942fd801b58d9a958ae
Tudor Roman <tudor@tudorr.ro>
2023-08-27 00:44:52 +0200

    Make emojis work in the terminal

--- modules/home-manager/desktop/fonts.nix
+++ modules/home-manager/desktop/fonts.nix
@@ -11,5 +11,34 @@ with lib; {
 
   config = mkIf cfg.enable {
     fonts.fontconfig.enable = true;
+    home.packages = [ pkgs.noto-fonts-emoji ];
+    xdg.configFile."fontconfig/conf.d/20-noto-emoji.conf".text = ''
+    <?xml version='1.0' encoding='UTF-8'?>
+    <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+    <fontconfig>
+     <alias>
+      <family>serif</family>
+      <prefer>
+       <family>Noto Sans</family>
+       <family>Noto Color Emoji</family>
+      </prefer>
+     </alias>
+     <alias>
+      <family>sans-serif</family>
+      <prefer>
+       <family>Noto Sans</family>
+       <family>Noto Color Emoji</family>
+      </prefer>
+     </alias>
+     <alias>
+      <family>monospace</family>
+      <prefer>
+       <family>Noto Sans</family>
+       <family>Noto Color Emoji</family>
+      </prefer>
+     </alias>
+     <dir>~/.fonts</dir>
+    </fontconfig>
+    '';
   };
 }
