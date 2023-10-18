6902d1256a3e6a5e21be0b610889c0289c933b29
Tudor Roman <tudor@tudorr.ro>
2023-10-18 13:38:27 +0200

    better indentation for fontconfig config

--- modules/home-manager/desktop/fonts.nix
+++ modules/home-manager/desktop/fonts.nix
@@ -6,44 +6,68 @@ with lib; {
   options = {
     homeModules.desktop.fonts = {
       enable = mkEnableOption "Enable fonts";
+      themeFont = mkOption {
+        default = {};
+        type = types.submodule ({ ... }: {
+          options = {
+            family = mkOption {
+              type = types.str;
+              default = "monospace";
+              example = "Fira Mono";
+            };
+            style = mkOption {
+              type = types.str;
+              default = "";
+              example = "Bold Semi-Condensed";
+            };
+            size = mkOption {
+              type = types.float;
+              default = 12.0;
+            };
+          };
+        });
+      };
     };
   };
 
   config = mkIf cfg.enable {
     fonts.fontconfig.enable = true;
+
     home.packages = with pkgs; [
       noto-fonts-emoji
     ];
+
     xdg.configFile."fontconfig/conf.d/20-noto-emoji.conf".text = ''
     <?xml version='1.0' encoding='UTF-8'?>
     <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
     <fontconfig>
-     <alias>
+      <alias>
       <family>emoji</family>
       <prefer>
         <family>Noto Color Emoji</family>
       </prefer>
-     </alias>
+      </alias>
 
-     <alias binding="weak">
-      <family>serif</family>
-      <prefer>
-       <family>emoji</family>
-      </prefer>
-     </alias>
-     <alias binding="weak">
-      <family>sans-serif</family>
-      <prefer>
-       <family>emoji</family>
-      </prefer>
-     </alias>
-     <alias binding="weak">
-      <family>monospace</family>
-      <prefer>
-       <family>emoji</family>
-      </prefer>
-     </alias>
-     <dir>~/.fonts</dir>
+      <alias binding="weak">
+        <family>serif</family>
+        <prefer>
+          <family>emoji</family>
+        </prefer>
+      </alias>
+
+      <alias binding="weak">
+        <family>sans-serif</family>
+        <prefer>
+          <family>emoji</family>
+        </prefer>
+      </alias>
+
+      <alias binding="weak">
+        <family>monospace</family>
+        <prefer>
+          <family>emoji</family>
+        </prefer>
+      </alias>
     </fontconfig>
     '';
   };
