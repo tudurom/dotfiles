8fbc483c82c140bfb9c8142bfebeb1c2157f26fa
Tudor Roman <tudor@tudorr.ro>
2023-12-27 16:19:43 +0200

    better nginx base config

--- modules/nixos/system/services/web/nginx.nix
+++ modules/nixos/system/services/web/nginx.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 let
   cfg = config.systemModules.services.web.nginx;
 in
@@ -11,12 +11,26 @@ with lib; {
       acceptTerms = true;
     };
 
+    # for checking various things: I recommend https://internet.nl/
     services.nginx = {
       enable = true;
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
       recommendedGzipSettings = true;
       recommendedOptimisation = true;
+
+      appendHttpConfig = let
+        # one year
+        maxAge = 31536000;
+      in ''
+        map $scheme $hsts_header {
+          https "max-age=${maxAge}";
+        }
+        add_header Strict-Transport-Security $hsts_header;
+
+        add_header Referrer-Policy "same-origin";
+        add_header X-Content-Type-Options nosniff;
+      '';
     };
 
     networking.firewall.allowedTCPPorts = [ 80 443 ];
